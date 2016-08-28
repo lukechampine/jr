cf54cc3d9a9922c28943328c431498ddc901892c
lukechampine <luke.champine@gmail.com>
2016-08-28 17:04:37 -0400

    add flags and formatting

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/rpc/jsonrpc"
 	"os"
 )
 
-var haveStdin bool = func() bool {
-	// detect whether the user is piping data to stdin
-	// (not perfect; doesn't work with e.g. /dev/zero)
-	stat, _ := os.Stdin.Stat()
-	return (stat.Mode() & os.ModeCharDevice) == 0
-}()
+func usage() {
+	fmt.Fprintln(os.Stderr, `USAGE:
+  jr ADDRESS:PORT METHOD [PARAMETER...]
+
+Options:
+  -no-format	do not format JSON output (default: false)`)
+}
 
 func die(args ...interface{}) {
 	fmt.Fprintln(os.Stderr, args...)
@@ -21,15 +24,23 @@ func die(args ...interface{}) {
 }
 
 func main() {
-	if len(os.Args) < 3 {
-		die(`USAGE:
-  jr ADDRESS:PORT METHOD [PARAMETER...]`)
+	ugly := flag.Bool("no-format", false, "do not format JSON output")
+	flag.Usage = usage
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 2 {
+		usage()
 	}
+
+	// detect whether the user is piping data to stdin
+	// (not perfect; doesn't work with e.g. /dev/zero)
+	stat, _ := os.Stdin.Stat()
+	haveStdin := (stat.Mode() & os.ModeCharDevice) == 0
 
 	// parse params, if supplied
 	var params json.RawMessage
-	if len(os.Args) > 3 {
-		params = json.RawMessage(os.Args[3])
+	if len(args) > 2 {
+		params = json.RawMessage(args[2])
 	} else if haveStdin {
 		stdin, err := ioutil.ReadAll(os.Stdin)
 		if err != nil {
@@ -39,7 +50,7 @@ func main() {
 	}
 
 	// connect to server
-	cli, err := jsonrpc.Dial("tcp", os.Args[1])
+	cli, err := jsonrpc.Dial("tcp", args[0])
 	if err != nil {
 		die("Couldn't connect to server:", err)
 	}
@@ -47,11 +58,20 @@ func main() {
 
 	// call
 	var reply json.RawMessage
-	err = cli.Call(os.Args[2], &params, &reply)
+	err = cli.Call(args[1], &params, &reply)
 	if err != nil {
 		die("Call failed:", err)
 	}
 
-	// print
-	fmt.Println(string(reply))
+	// print response, formatting if requested
+	if *ugly {
+		fmt.Println(string(reply))
+	} else {
+		buf := new(bytes.Buffer)
+		err = json.Indent(buf, []byte(reply), "", "\t")
+		if err != nil {
+			die("Couldn't format response:", err)
+		}
+		fmt.Println(buf)
+	}
 }
